6eb3f6998534fde4d66160a58e4a96c46e7c99bf
allenn66772 <allenn4124@gmail.com>
2026-01-15 10:46:06 +0530

    completed

--- src/services/serverURL.js
+++ src/services/serverURL.js
@@ -1,6 +1,6 @@
 // const SERVERURL="http://localhost:3000"
 
-const SERVERURL="https://book-store-backend-ofjj.onrender.com"
+const SERVERURL="https://book-store-backend-gm68.onrender.com"
 
 
 export default SERVERURL
